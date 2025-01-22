a4b4a22fab9f144ecdfdc23bff352d02abd412d3
1U23IT016CHANDRU <139447715+2023ITACHANDRU@users.noreply.github.com>
2025-01-22 08:42:10 +0000

    DONE5

--- LinkedList.java
+++ LinkedList.java
@@ -1,120 +1,62 @@
 import java.util.Scanner;
 
 public class LinkedList {
+
     class Node {
-            int data;
-                    Node next;
-                            public Node(int data) {
-                                        this.data = data;
-                                                    this.next = null;
-                                                            }
-                                                                }
+        int data;
+        Node next;
 
-                                                                   Node head = null;
-                                                                        Node tail = null;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
-                                                                            public void addNode(int data) {
-                                                                                    Node newNode = new Node(data);
-                                                                                            if (head == null) {
-                                                                                                        head = newNode;
-                                                                                                                    tail = newNode;
-                                                                                                                            } else {
-                                                                                                                                        tail.next = newNode;
-                                                                                                                                                    tail = newNode;
-                                                                                                                                                            }
-                                                                                                                                                                }
+    Node head = null;
+    Node tail = null;
 
-                                                                                                                                                                    public int length() {
-                                                                                                                                                                            int count = 0;
-                                                                                                                                                                                    Node current = head;
-                                                                                                                                                                                            while (current != null) {
-                                                                                                                                                                                                        count++;
-                                                                                                                                                                                                                    current = current.next;
-                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                    return count;
-                                                                                                                                                                                                                                        }
+    public void addNode(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
 
-                                                                                                                                                                                                                                            public void displayList() {
-                                                                                                                                                                                                                                                    Node current = head;
-                                                                                                                                                                                                                                                            while (current != null) {
-                                                                                                                                                                                                                                                                        System.out.print(current.data + " -> ");
-                                                                                                                                                                                                                                                                                    current = current.next;
-                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                    System.out.println("null");
-                                                                                                                                                                                                                                                                                                        }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
+        while (scanner.hasNextInt()) {
+            int data = scanner.nextInt();
+            list.addNode(data);
+        }
+        System.out.println("Linked List created");
+        System.out.println("Linked List");
+        list.displayList();
+        System.out.println("Length of the Linked List is:" + list.length());
+        scanner.close();
+    }
 
-                                                                                                                                                                                                                                                                                                            public static void main(String[] args) {
-                                                                                                                                                                                                                                                                                                                    LinkedList list = new LinkedList();
-                                                                                                                                                                                                                                                                                                                            Scanner scanner = new Scanner(System.in);
-                                                                                                                                                                                                                                                                                                                                    System.out.println("Enter integers to create nodes (enter a non-integer to exit):");
-                                                                                                                                                                                                                                                                                                                                            while (scanner.hasNextInt()) {
-                                                                                                                                                                                                                                                                                                                                                        int data = scanner.nextInt();
-                                                                                                                                                                                                                                                                                                                                                                    list.addNode(data);
-                                                                                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                                                                                   System.out.println("Linked List createdLinked List");
-                                                                                                                                                                           
-                                                                                                                                                                                                                                                                                                                                                                                                    list.displayList();
-                                                                                                                                                                                                                                                                                                                                                                                                            System.out.println("Length of the Linked List is:" + list.length());
-                                                                                                                                                                                                                                                                                                                                                                                                                    scanner.close();
-                                                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                                        }
+    int length() {
+        int count = 0;
+        Node current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
 
-
-
-
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                                                                    
-
-
-
-
-
-
-
-
-
-
-
-
+    public void displayList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
